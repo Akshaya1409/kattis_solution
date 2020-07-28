cc095bdd8c8bc6913764b55a617ff8a25605fcfd
Akshaya1409 <67379624+Akshaya1409@users.noreply.github.com>
2020-07-28 19:00:44 +0530

    Difficulty: 1.6

---
+++ Help a PhD candid.py
@@ -0,0 +1,26 @@
+N=int(input())
+lst=[]
+for i in range(N):
+   s=input("")
+   if len(s)==3:
+     lst.append(s)
+for l in lst:
+   if l=="P=NP":
+     print("skipped")
+   else:
+     tot=int(l[0])+int(l[2])
+     print(tot)
+     
+     
+     # Gives a runtime error ---> We did not split the addition form 
+     
+N= int(input())
+for i in range(N):
+  s = input()
+  if s == "P=NP":
+    print("skipped")
+  else:
+    l,m = s.split("+")
+    l1 = int(l)
+    m1 = int(m)
+    print(l1 + m1)
