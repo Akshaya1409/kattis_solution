4a9491cb0c0299689e9a75f8fca7dd2342b583a4
Akshaya B <67379624+Akshaya1409@users.noreply.github.com>
2021-10-06 18:19:13 +0530

    Create forests.py

---
+++ forests.py
@@ -0,0 +1,9 @@
+p,t = map(int,input().split())
+opinions = [set() for _ in range(p)]
+try:
+  for _ in range(p*t):
+    _p,_t = map(int,input().split())
+    opinions[_p-1].add(_t)
+except EOFError:
+    pass
+print(len({frozenset(s) for s in opinions}))
